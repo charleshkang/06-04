0795a787953e2af01d7d5c8bd4e1e6363aeaab4b
charles kang <ckangkitchen@gmail.com>
2015-06-06 11:05:14 -0400

    first commit

---
+++ Variables2/Variables2/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Variables2
+//
+//  Created by Charles Kang on 6/4/15.
+//  Copyright (c) 2015 Charles Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    return 0;NSString
+
+}
---
+++ Variables2/Variables2/main2.m
@@ -0,0 +1,11 @@
+
+//
+//  main2.m
+//  Variables2
+//
+//  Created by Charles Kang on 6/4/15.
+//  Copyright (c) 2015 Charles Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
